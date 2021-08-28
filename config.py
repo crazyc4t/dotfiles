1c16b00864dfa005423db7c892c1e97eeffab5ea
Said Neder <saidneder1@gmail.com>
2021-08-27 21:44:31 -0500

    my first dotfiles commit

---
+++ config.py
@@ -0,0 +1,284 @@
+# Copyright (c) 2010 Aldo Cortesi
+# Copyright (c) 2010, 2014 dequis
+# Copyright (c) 2012 Randall Ma
+# Copyright (c) 2012-2014 Tycho Andersen
+# Copyright (c) 2012 Craig Barnes
+# Copyright (c) 2013 horsik
+# Copyright (c) 2013 Tao Sauvage
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from typing import List  # noqa: F401
+from libqtile import bar, layout, widget
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.lazy import lazy
+
+mod = "mod4"  # Supr key
+
+# Colors
+
+focus = "000066"  # blue
+dark = "0f101a"
+light = "f1ffff"
+inactive = "404040"
+
+keys = [
+
+    # ----------------------- QTILE WINDOW MANAGMENT ------------------------ #
+
+    # Switch between windows
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "space", lazy.layout.next(),
+        desc="Move window focus to other window"),
+
+    # Move windows between left/right columns or move up/down in current stack.
+    # Moving out of range in Columns layout will create new column.
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
+        desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
+        desc="Move window to the right"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(),
+        desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+
+    # Grow windows. If current window is on the edge of screen and direction
+    # will be to screen edge - window would shrink.
+    Key([mod, "control"], "h", lazy.layout.grow_left(),
+        desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(),
+        desc="Grow window to the right"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(),
+        desc="Grow window down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+
+    # Toggle between different layouts as defined below
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+
+    # ------------------ APPS HOTKEYS --------------------- #
+
+    # Launch Terminal
+    Key([mod], "Return", lazy.spawn("alacritty")),
+
+    # Menu
+    Key([mod], "m", lazy.spawn("rofi -show run")),
+
+    # Window Nav
+    Key([mod, "shift"], "m", lazy.spawn("rofi -show")),
+
+    # Volume
+    Key([], "XF86AudioLowerVolume", lazy.spawn(
+        "pactl set-sink-volume @DEFAULT_SINK@ -10%"
+    )),
+    Key([], "XF86AudioRaiseVolume", lazy.spawn(
+        "pactl set-sink-volume @DEFAULT_SINK@ +10%"
+    )),
+    Key([], "XF86AudioMute", lazy.spawn(
+        "pactl set-sink-mute @DEFAULT_SINK@ toggle"
+    )),
+
+    # Brightness
+    Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl set +10%")),
+    Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set 10%-")),
+
+    # Screenshot
+    Key([], "Print", lazy.spawn("spectacle")),
+
+    # Web browser
+    Key([mod], "f", lazy.spawn("firefox")),
+
+    # Keyboard Layouts
+    Key([mod], "z", lazy.spawn("setxkbmap es olpc")),
+    Key([mod], "x", lazy.spawn("setxkbmap us")),
+
+    # Toggle Floating
+    Key([mod, "shift"], "f", lazy.window.toggle_floating()),
+
+    # Redshift
+    Key([mod], "r", lazy.spawn("redshift -O 4000k")),
+    Key([mod, "shift"], "r", lazy.spawn("redshift -x")),
+
+    # Discord
+    Key([mod], "d", lazy.spawn("discord")),
+
+    # Spotify
+    Key([mod], "s", lazy.spawn("spotify")),
+
+    # Pavucontrol
+    Key([mod], "p", lazy.spawn("pavucontrol")),
+
+    # Thunar
+    Key([mod], "t", lazy.spawn("thunar")),
+
+    # Atom Text Editor
+    Key([mod], "a", lazy.spawn("atom")),
+
+]
+
+# 1 = Firefox | 2 = Terminal | 3 = Programming | 4 = File Explorer | 5 = Discord | 6 = Spotify | 7 = School #
+
+groups = [Group(i) for i in [
+    "  ", "  ", "  ", "  ", " ﭮ ", " 阮 ", "  ",
+]]
+
+for i, group in enumerate(groups):
+    actual_key = str(i + 1)
+    keys.extend([
+        # Switch to workspace N
+        Key([mod], actual_key, lazy.group[group.name].toscreen()),
+        # Send window to workspace N
+        Key([mod, "shift"], actual_key, lazy.window.togroup(group.name))
+    ])
+
+# Preferred Layouts = MonadTall | Matrix | Max | MonadWide (In order of usage)
+
+layout_conf = {
+    "border_focus": focus
+}
+
+layouts = [
+    # layout.Columns(border_focus_stack='#d75f5f'),
+    layout.MonadTall(**layout_conf),
+    # Try more layouts by unleashing below layouts.
+    # layout.Stack(num_stacks=2),
+    # layout.Bsp(),
+    layout.Matrix(**layout_conf),
+    layout.Max(),
+    layout.MonadWide(**layout_conf),
+    # layout.RatioTile(),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
+]
+
+# Font
+font = "Hack Nerd Font"
+
+widget_defaults = dict(
+    font=font,
+    fontsize=16,
+    padding=3,
+)
+extension_defaults = widget_defaults.copy()
+
+# Widgets
+
+widget_conf = {
+
+}
+
+screens = [
+    Screen(
+        top=bar.Bar(
+            [
+                widget.GroupBox(
+                    font=font,
+                    fontsize=19,
+                    margin_y=3,
+                    margin_x=0,
+                    padding_y=8,
+                    padding_x=5,
+                    borderwidth=1,
+                    active=light,
+                    inactive=inactive,
+                    rounded=False,
+                    highlight_method="block",
+                    urgent_alert_method="block",
+                    urgent_border=dark,
+                    this_current_screen_border=focus,
+                    this_screen_border=dark,
+                    other_current_screen_border=dark,
+                    other_screen_border=dark,
+                    disable_drag=True
+                ),
+                widget.WindowName(),
+                widget.CurrentLayout(),
+                widget.TextBox("Never Settle"),
+                widget.Systray(),
+                widget.KeyboardLayout(
+                    configured_keyboards=["us", "es olpc"],
+                    display_map={"us": "us", "es olpc": "es"}
+                ),
+                widget.Clock(format='%m-%d %a %I:%M %p'),
+            ],
+            24,
+            opacity=0.70
+        ),
+    ),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: List
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(float_rules=[
+    # Run the utility of `xprop` to see the wm class and name of an X client.
+    *layout.Floating.default_float_rules,
+    # gitk
+    Match(
+        wm_class='confirmreset'),
+    # gitk
+    Match(
+        wm_class='makebranch'),
+    # gitk
+    Match(
+        wm_class='maketag'),
+    # ssh-askpass
+    Match(
+        wm_class='ssh-askpass'),
+    # gitk
+    Match(
+        title='branchdialog'),
+    # GPG key password entry
+    Match(
+        title='pinentry'),
+])
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
+
+# Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
