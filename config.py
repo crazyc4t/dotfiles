2b844f40d048d5e6f4fc2e2278634881c59389c8
Said Neder <saidneder1@gmail.com>
2021-08-27 23:44:55 -0500

    My first dotfiles commit

--- config.py
+++ config.py
@@ -1,30 +1,11 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# / __| __ _ (_) __| |      | \| | ___  __| | ___  _ _
+# \__ \/ _` || |/ _` |      | .  |/ -_)/ _` |/ -_)| '_|
+# |___/\__/_||_|\__/_|      |_|\_|\___|\__/_|\___||_|
 
-from typing import List  # noqa: F401
+# --------------- Qtile Conf ----------------#
+# https://docs.qtile.org/en/latest/index.html#
+
+from typing import List
 from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
@@ -33,11 +14,12 @@ mod = "mod4"  # Supr key
 
 # Colors
 
-focus = "000066"  # blue
+focus = "0000ff"  # blue
 dark = "0f101a"
 light = "f1ffff"
 inactive = "404040"
-
+purple = "6600ff"
+turquese = "00ccff"
 keys = [
 
     # ----------------------- QTILE WINDOW MANAGMENT ------------------------ #
@@ -154,8 +136,11 @@ for i, group in enumerate(groups):
 
 # Preferred Layouts = MonadTall | Matrix | Max | MonadWide (In order of usage)
 
+
 layout_conf = {
-    "border_focus": focus
+    "border_focus": focus,
+    "margin": 5,
+    "border_width": 2
 }
 
 layouts = [
@@ -185,10 +170,6 @@ extension_defaults = widget_defaults.copy()
 
 # Widgets
 
-widget_conf = {
-
-}
-
 screens = [
     Screen(
         top=bar.Bar(
@@ -214,14 +195,16 @@ screens = [
                     disable_drag=True
                 ),
                 widget.WindowName(),
-                widget.CurrentLayout(),
-                widget.TextBox("Never Settle"),
                 widget.Systray(),
                 widget.KeyboardLayout(
                     configured_keyboards=["us", "es olpc"],
                     display_map={"us": "us", "es olpc": "es"}
                 ),
-                widget.Clock(format='%m-%d %a %I:%M %p'),
+                widget.CurrentLayout(),
+                widget.TextBox(text="Never Settle"),
+                widget.Clock(
+                    format='%m-%d %a %I:%M %p',
+                    padding=5),
             ],
             24,
             opacity=0.70
@@ -273,12 +256,7 @@ reconfigure_screens = True
 # focus, should we respect this or not?
 auto_minimize = True
 
-# Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
+
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
 wmname = "LG3D"
